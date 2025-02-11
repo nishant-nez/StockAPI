19549053ba8fec59503922a6b808431f0bb22542
nishant-nez <nishant.khadka@deerwalk.edu.np>
2025-02-11 21:34:20 +0545

    Add project files.

---
+++ StockAPI/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿namespace StockAPI.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        // navigation property - allows to access the stock properties
+        public Stock? Stock { get; set; }
+    }
+}
